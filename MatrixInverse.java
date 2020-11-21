596f743664eb1010effd69771b8514cdaae3f3b9
aarnich <aaron.gmpc@gmail.com>
2020-11-22 00:22:45 +0800

    modified methods

--- MatrixInverse.java
+++ MatrixInverse.java
@@ -3,8 +3,13 @@ public class MatrixInverse {
         return matrix_i;
     }
 
-    public void GenerateIdentityMatrix(int[][] matrix_i) {
-
+    public static int[][] GenerateIdentityMatrix(int[][] matrix_i) {
+        int[][] identity = new int[matrix_i.length][matrix_i[0].length];
+        int index_i = 0;
+        for (int[] tuple : identity) {
+            tuple[index_i] = 1;
+            index_i += 1;
+        }
+        return identity;
     }
-
 }
